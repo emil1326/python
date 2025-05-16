c402f0d63e5d2b38c97ddf5a83b035ceb928822e
emil1326 <116656753+emil1326@users.noreply.github.com>
2025-05-16 15:21:33 -0400

    fix matrice

--- Pathing/martices.py
+++ Pathing/martices.py
@@ -15,18 +15,18 @@ matrice_distances_test = [
 
 matrice_distances_floor = [
     #  1    2    3    4    5    6    7    8    9   10   11   12   13   14
-    [  0,   1, Inf, Inf,   1, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf],  # 1
-    [  1,   0,   1, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf],  # 2
-    [Inf,   1,   0,   1, Inf, Inf,   1, Inf, Inf, Inf, Inf, Inf, Inf, Inf],  # 3
-    [Inf, Inf,   1,   0, Inf, Inf, Inf, Inf,   1, Inf, Inf, Inf, Inf, Inf],  # 4
-    [  1, Inf, Inf, Inf,   0,   1, Inf, Inf, Inf,   1, Inf, Inf, Inf, Inf],  # 5
-    [Inf, Inf, Inf, Inf,   1,   0,   1, Inf, Inf, Inf,   1, Inf, Inf, Inf],  # 6
-    [Inf, Inf,   1, Inf, Inf,   1,   0,   1, Inf, Inf, Inf,   1, Inf, Inf],  # 7
-    [Inf, Inf, Inf, Inf, Inf, Inf,   1,   0,   1, Inf, Inf, Inf,   1, Inf],  # 8
-    [Inf, Inf, Inf,   1, Inf, Inf, Inf,   1,   0, Inf, Inf, Inf, Inf,   1],  # 9
-    [Inf, Inf, Inf, Inf,   1, Inf, Inf, Inf, Inf,   0,   1,   1, Inf, Inf],  # 10
-    [Inf, Inf, Inf, Inf, Inf,   1, Inf, Inf, Inf,   1,   0, Inf,   1, Inf],  # 11
-    [Inf, Inf, Inf, Inf, Inf, Inf,   1, Inf, Inf,   1, Inf,   0,   1, Inf],  # 12
-    [Inf, Inf, Inf, Inf, Inf, Inf, Inf,   1, Inf, Inf,   1,   1,   0,   1],  # 13
-    [Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf,   1, Inf, Inf, Inf,   1,   0],  # 14
+    [  0,   2, Inf, Inf, Inf,   3, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf],  # 1
+    [  2,   0,   2, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf],  # 2
+    [Inf,   2,   0,   4, Inf, Inf,   2, Inf, Inf, Inf, Inf, Inf, Inf, Inf],  # 3
+    [Inf, Inf,   4,   0, Inf, Inf, Inf, Inf,   2, Inf, Inf, Inf, Inf, Inf],  # 4
+    [Inf, Inf, Inf, Inf,   0,   3, Inf, Inf, Inf,   2, Inf, Inf, Inf, Inf],  # 5
+    [  3, Inf, Inf, Inf,   3,   0, Inf, Inf, Inf, Inf,   2, Inf, Inf, Inf],  # 6
+    [Inf, Inf,   2, Inf, Inf, Inf,   0,   2, Inf, Inf, Inf, Inf,   4, Inf],  # 7
+    [Inf, Inf, Inf, Inf, Inf, Inf,   2,   0,   2, Inf, Inf, Inf, Inf,   4],  # 8
+    [Inf, Inf, Inf,   2, Inf, Inf, Inf,   2,   0, Inf, Inf, Inf, Inf, Inf],  # 9
+    [Inf, Inf, Inf, Inf,   2, Inf, Inf, Inf, Inf,   0,   3,   8, Inf, Inf],  # 10
+    [Inf, Inf, Inf, Inf, Inf,   2, Inf, Inf, Inf,   3,   0,   3, Inf, Inf],  # 11
+    [Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf,   8,   3,   0,   2, Inf],  # 12
+    [Inf, Inf, Inf, Inf, Inf, Inf,   4, Inf, Inf, Inf, Inf,   2,   0,   2],  # 13
+    [Inf, Inf, Inf, Inf, Inf, Inf, Inf,   4, Inf, Inf, Inf, Inf,   2,   0],  # 14
 ]
