8e49979e5fc5b2b2d0eaf1ca71b50d3688311307
emil1326 <emilien.devauchelle@outlook.com>
2025-11-17 10:56:40 -0500

    Add lidar class with Board sub-class and basic methods

---
+++ labs_ses2/lidar.py
@@ -0,0 +1,23 @@
+
+class lidar:
+    class Board:
+        def __init__(self) -> None:
+            pass
+        
+        def rescale(self, amount) -> "lidar.Board":
+            return amount
+        
+        def displace(self, amount) -> "lidar.Board":
+            return amount
+        
+    def __init__(self) -> None:
+        pass
+    
+    def startReading(self) -> None:
+        pass
+    
+    def stopReading(self) -> None:
+        pass
+    
+    def getBoardDistance(self) -> float:
+        return 0.0
